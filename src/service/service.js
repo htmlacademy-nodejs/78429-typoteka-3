51aff7a1c67273335d24fcf7bb5d390e7207ff8d
Andrew Nefedov <ekzersis91@yandex.ru>
2022-03-23 18:06:55 +0300

    2.13. Сервер наносит первый удар

--- src/service/service.js
+++ src/service/service.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const http = require(`http`);
 const fs = require(`fs`).promises;
 const path = require(`path`);
 const rootPath = process.cwd();
@@ -61,15 +62,15 @@ const generateMock = async (count) => {
 
   let publications = [];
   for (let x = 0; x < count; x++) {
-    const title = titles[randomInteger(0, titles.length)];
+    const title = titles[randomInteger(0, titles.length - 1)];
     const announce = sentences[randomInteger(0, sentences.length)];
     const fullText = Array.from(Array(5).keys()).reduce(((text) => {
-      text += sentences[randomInteger(0, sentences.length)] + ` `;
+      text += sentences[randomInteger(0, sentences.length - 1)] + ` `;
       return text;
     }), ``);
 
     const createdDate = dayjs.between(dayjs().subtract(3, `month`), dayjs()).format(`YYYY-MM-DD hh:mm:ss`);
-    const сategory = сategories[randomInteger(0, сategories.length)];
+    const сategory = сategories[randomInteger(0, сategories.length - 1)];
 
     publications.push({
       title,
@@ -88,15 +89,79 @@ const generateMock = async (count) => {
   }
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(`mock.json`);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+
+      break;
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      break;
+  }
+};
+
 
 program
+  .option(`-s, --server [port]`)
   .option(`--version`)
   .option(`--help`)
   .option(`--generate [count]`);
 
+
 program.parse(process.argv);
 
 const options = program.opts();
+
+if (options.server) {
+  if (typeof options.server === `boolean` && options.server) {
+    options.server = 3000;
+  }
+  const port = options.server;
+  http.createServer(onClientConnect)
+    .listen(port)
+    .on(`listening`, () => {
+      console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    })
+    .on(`error`, (err) => {
+      console.error(chalk.red(`Ошибка при создании сервера: ${err}`));
+    });
+}
 
 if (options.version) {
   log(chalk.blue(packageData.version));
