1311e5add8cf50a75db867148b345a32a3a69bc1
Andrew Nefedov <ekzersis91@yandex.ru>
2022-05-20 12:39:03 +0300

    fix

--- schema.sql
+++ schema.sql
@@ -1,23 +1,14 @@
 DROP TABLE IF EXISTS roles CASCADE;
-
 DROP TABLE IF EXISTS users CASCADE;
-
 DROP TABLE IF EXISTS categories CASCADE;
-
 DROP TABLE IF EXISTS articles CASCADE;
-
 DROP TABLE IF EXISTS comments;
-
 DROP TABLE IF EXISTS articles_categories;
-
 CREATE TABLE roles(
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(10) NOT NULL
 );
-
-ALTER TABLE
-    roles OWNER TO academy;
-
+ALTER TABLE roles OWNER TO academy;
 CREATE TABLE users(
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email varchar(250) UNIQUE NOT NULL,
@@ -27,23 +18,15 @@ CREATE TABLE users(
     avatar varchar(50) NOT NULL,
     role_id integer NOT NULL DEFAULT 1,
     FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE
-    SET
-        DEFAULT ON UPDATE
-    SET
-        DEFAULT
+    SET DEFAULT ON UPDATE
+    SET DEFAULT
 );
-
-ALTER TABLE
-    users OWNER TO academy;
-
+ALTER TABLE users OWNER TO academy;
 CREATE TABLE categories(
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(30) NOT NULL
 );
-
-ALTER TABLE
-    categories OWNER TO academy;
-
+ALTER TABLE categories OWNER TO academy;
 CREATE TABLE articles(
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id integer NOT NULL,
@@ -54,10 +37,7 @@ CREATE TABLE articles(
     created_at timestamp DEFAULT current_timestamp,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-ALTER TABLE
-    articles OWNER TO academy;
-
+ALTER TABLE articles OWNER TO academy;
 CREATE TABLE comments(
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id integer NOT NULL,
@@ -67,10 +47,7 @@ CREATE TABLE comments(
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-ALTER TABLE
-    comments OWNER TO academy;
-
+ALTER TABLE comments OWNER TO academy;
 CREATE TABLE articles_categories(
     article_id integer NOT NULL,
     category_id integer NOT NULL,
@@ -78,8 +55,5 @@ CREATE TABLE articles_categories(
     FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-ALTER TABLE
-    articles_categories OWNER TO academy;
-
+ALTER TABLE articles_categories OWNER TO academy;
 CREATE INDEX ON articles(title);
